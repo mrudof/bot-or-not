0e0143bc651dedd493d82ffb9fecb69d5ccf17f4
Anders Tunold Krakenes <tunold@gmail.com>
2016-09-24 19:49:57 -0400

    Debug quest logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games do
+    get '/users/choose', to: 'users#choose'
     resources :rounds do
       resources :quests
     end
@@ -11,7 +12,6 @@ Rails.application.routes.draw do
   resources :quests do
     resources :quest_members
     #route to update quests_chosen for a user
-    get '/users', to: 'quests#choose'
   end
-  
+
 end
