86e1a2c9dad5a16906883a0744a03ef14c9a3cdb
Melissa Rudofsky <melissarudofsky@gmail.com>
2016-09-27 22:51:37 -0400

    added character names

--- app/assets/javascripts/components/good_card.es6.jsx
+++ app/assets/javascripts/components/good_card.es6.jsx
@@ -3,17 +3,15 @@ class GoodCard extends React.Component {
   render () {
     let showChar
     let showName
+    showName = <p> You are {this.props.currentUser.character}!</p>
     if (this.props.currentUser.character === "Merlin") {
-        showName =
-        <h3> You are Merlin. The Evil people are: </h3>
         showChar =
         this.props.users.map((user ,i) => {
         if (user.good === false && user.character != "Mordred") {
-          return (<li key={i}>{user.name}</li>)
+          return (<li key={i}>{user.name} is evil.</li>)
         }
       })
     } else if ((this.props.currentUser.character === "Percival")) {
-      showName =  <h3> You are Percival. </h3>
       showChar = this.props.users.map((user ,i) => {
       if (user.character === "Merlin" || user.character === "Morgana") {
         return (<li key={i}>{user.name}</li>)
