634d7f97b95de417cad99914a230cc57ba8d566a
Anders Tunold Krakenes <tunold@gmail.com>
2016-09-29 18:41:43 -0400

    added pics

--- app/assets/javascripts/components/evil_card.es6.jsx
+++ app/assets/javascripts/components/evil_card.es6.jsx
@@ -1,10 +1,12 @@
 class EvilCard extends React.Component {
 
   render () {
+    showPic = <img src={`/${this.props.currentUser.character}.jpg`} className="img-responsive" />
     showName = <p> You are {this.props.currentUser.character}!</p>
     return(
       <div>
-        <h2>... is EVIL!!</h2>
+        {showPic}
+        {showName} <h2>secretly a robot.</h2>
         <p> and so is.. </p>
         <ul>
         {
@@ -14,7 +16,8 @@ class EvilCard extends React.Component {
             }
           })
         }
-        {showName}
+
+
         </ul>
       </div>
     )
--- app/assets/javascripts/components/good_card.es6.jsx
+++ app/assets/javascripts/components/good_card.es6.jsx
@@ -3,26 +3,39 @@ class GoodCard extends React.Component {
   render () {
     let showChar
     let showName
+    let showPic
+    let showText
+    showPic = <img src={`/${this.props.currentUser.character}.jpg`} className="img-responsive img-rounded" />
+
     showName = <p> You are {this.props.currentUser.character}!</p>
-    if (this.props.currentUser.character === "Merlin") {
+
+    if (this.props.currentUser.character === "Sarah Connor") {
+
+        showText = <h5>The following are secretly robots: </h5>
+
         showChar =
         this.props.users.map((user ,i) => {
-        if (user.good === false && user.character != "Mordred") {
-          return (<li key={i}>{user.name} is evil.</li>)
+        if (user.good === false && user.character != "Boomer") {
+          return (<li key={i}>{user.name}</li>)
         }
       })
-    } else if ((this.props.currentUser.character === "Percival")) {
+    } else if ((this.props.currentUser.character === "Deckard")) {
+
+      showText = <h5>You know one of these players is Sarah Connor and one is Rachael. </h5>
+
       showChar = this.props.users.map((user ,i) => {
-      if (user.character === "Merlin" || user.character === "Morgana") {
+      if (user.character === "Sarah Connor" || user.character === "Rachael") {
         return (<li key={i}>{user.name}</li>)
       }
     })
     }
     return(
       <div>
-        <h2>... is GOOD!!</h2>
+        {showPic}
+        {showName} <h2>a human.</h2>
         <ul>
-        {showName}
+
+        {showText}
         {showChar}
         </ul>
       </div>
