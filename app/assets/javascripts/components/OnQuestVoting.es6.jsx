407d1bf3adaf1a1ae6eae7b880aa4dd86dd61434
Melissa Rudofsky <melissarudofsky@gmail.com>
2016-09-29 12:13:33 -0400

    Update OnQuestVoting.es6.jsx

--- app/assets/javascripts/components/OnQuestVoting.es6.jsx
+++ app/assets/javascripts/components/OnQuestVoting.es6.jsx
@@ -9,13 +9,13 @@ class OnQuestVoting extends React.Component {
     if (currentUserIsQuestMembersArray.length) {
       succedQuest =
       <div>
-        <p>Yes, you are on a mission. DELETE ME!!!</p>
+   
         <YouAreOnAQuest memberID={currentUserIsQuestMembersArray[0].id} currentGame={this.props.currentGame} currentQuest={this.props.currentQuest} members={this.props.members} currentUser={this.props.currentUser} users={this.props.users} />
       </div>
     } else {
       succedQuest =
       <div>
-        <p>You are not on this mission. DELETE ME!</p>
+
         <WaitingForQuestResult currentGame={this.props.currentGame} currentQuest={this.props.currentQuest} members={this.props.members} currentUser={this.props.currentUser} users={this.props.users} />
       </div>
     }
