b60fe55f9729d0509ca9c28f59dadfeafdfec6fa
mrudof <melissarudofsky@gmail.com>
2016-09-29 10:25:24 -0400

    cleaned up some headers and added increased timer

--- app/assets/javascripts/components/OnQuestVoting.es6.jsx
+++ app/assets/javascripts/components/OnQuestVoting.es6.jsx
@@ -9,20 +9,19 @@ class OnQuestVoting extends React.Component {
     if (currentUserIsQuestMembersArray.length) {
       succedQuest =
       <div>
-        <p>Yes, you are on a quest</p>
+        <p>You are on this quest</p>
         <YouAreOnAQuest memberID={currentUserIsQuestMembersArray[0].id} currentGame={this.props.currentGame} currentQuest={this.props.currentQuest} members={this.props.members} currentUser={this.props.currentUser} users={this.props.users} />
       </div>
     } else {
       succedQuest =
       <div>
-        <p>You are not on a quest</p>
+        <p>You are not on this quest</p>
         <WaitingForQuestResult currentGame={this.props.currentGame} currentQuest={this.props.currentQuest} members={this.props.members} currentUser={this.props.currentUser} users={this.props.users} />
       </div>
     }
 
     return(
       <div>
-        <h3>On quest voting: did the quest succeed or fail?!?</h3>
         {succedQuest}
       </div>
     )
