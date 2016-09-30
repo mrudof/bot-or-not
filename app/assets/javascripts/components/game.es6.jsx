80684f8844eb87f93eb2de2c3540bf89dfafe5d4
Adam Spittler <spittler@gmail.com>
2016-09-29 20:44:18 -0400

    add rules

--- app/assets/javascripts/components/game.es6.jsx
+++ app/assets/javascripts/components/game.es6.jsx
@@ -1,10 +1,5 @@
 class Game extends React.Component {
-
-
-
   render () {
-
-
       return(
       <div className="container-fluid">
         <SelectQuestSplits users={this.props.gameUsers} currentUser={this.props.currentUser} currentGame={this.props.currentGame} currentRound={this.props.currentRound} />
--- app/assets/javascripts/components/rules.es6.jsx
+++ app/assets/javascripts/components/rules.es6.jsx
@@ -3,7 +3,8 @@ class Rules extends React.Component {
     return(
       <div>
         <h1>The Rules of the game</h1>
-        <p>(Placeholder)</p>
+        <p>Robots that are indistinguashable from humans have infiltrated humanity. They are still in the minority and are secretly sabotaging human's missions.</p>
+        <p>Humans win by successfully completing three missions. Robots win by sabotaging three missions.</p>
       </div>
     )
   }
