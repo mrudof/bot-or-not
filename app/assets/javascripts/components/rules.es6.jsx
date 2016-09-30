a4b93769a032683fc3b70e69e015b55251a5bd77
Anders Tunold Kråkenes <tunold@gmail.com>
2016-09-30 02:02:29 -0400

    Update rules.es6.jsx

--- app/assets/javascripts/components/rules.es6.jsx
+++ app/assets/javascripts/components/rules.es6.jsx
@@ -3,7 +3,7 @@ class Rules extends React.Component {
     return(
       <div>
         <h1>The Rules of the game</h1>
-        <p>Robots that are indistinguishable from humans have infiltrated humanity. They are plotting against us. While they are in the minority, they have one powerful asset: they know who eachother are.</p>
+        <p>Robots that are indistinguishable from humans have infiltrated humanity. They are plotting against us. While they are in the minority, they have one powerful asset: they know who each other are.</p>
         <p>Humans must complete three missions in order to prevail. Robots win by sabotaging three missions.</p>
       </div>
     )
